29e8fe65c5bfc08808ded40142ced67cf4506fc3
chrischne <chrischne@gmail.com>
2018-09-25 17:11:14 +0200

    initial beeswarm chart setup

--- js/charts.js
+++ js/charts.js
@@ -284,3 +284,147 @@ function constrainedLayoutGraph(parentId, graphbase, options) {
     }
 
 }
+
+
+function beeswarm(parentId, file, options) {
+
+    var colors1 = ['#324D5C', '#46B29D', '#F0CA4D', '#E37B40', '#DE5B49', 'black'];
+    var colors2 = ['#2C594F', '#E54661', '#FFA644', '#998A2F', '#002D40', 'black'];
+    var colors3 = ['#F15B2A', '#2A9FBC', '#404040', '#A62E5C', '#9BC850', 'black'];
+
+    var parseDate = d3.timeParse("%d-%m-%Y"); //19-4-2018 //2017-11-20
+
+    var rScale = d3.scalePow();
+    var xScale = null;
+    var yScale = null;
+    var freqScale = d3.scaleOrdinal();
+    var colScale = d3.scaleOrdinal()
+        .domain([0, 1, 2, 3, 4, 5])
+        .range(colors2);
+
+    var companyScale = d3.scalePoint();
+    var nameScale = d3.scalePoint();
+    var refScale = d3.scaleLinear();
+    var dateScale = d3.scaleLinear();
+    var percent = d3.scaleLinear();
+    var locationScale = d3.scalePoint();
+    var ratingScale = d3.scalePoint();
+    var originScale = d3.scalePoint();
+
+
+    var rAcc = null;
+
+    //var file = 'assets/data/flavors_of_cacao.csv'; //'Short4Chris_Tonal3.csv' //'./Tinnitus_Hub_Survey.csv';
+    var data = [];
+    var ndata = [];
+    var xFilter = 'all';
+    var yFilter = 'all';
+    var rFilter = 'all';
+    var ready = false;
+
+    var border = 100;
+    var minR = 0.2;
+    var maxR = 8;
+
+    var simulation = null;
+    var kn = 1;
+    var axisSvg = null;
+    var nTicks = 10;
+    var keys = null;
+
+    var dia = 3;
+
+    var selectedBubbles = [];
+
+    var sketch = function (s) {
+
+        var x = 100;
+        var y = 100;
+
+        s.setup = function () {
+
+            let w = options && options.width ? options.width : 600;
+            let h = options && options.height ? options.height : 600;
+
+            // let w = 600;
+            // let h = 600;
+
+            s.createCanvas(w, h);
+
+
+            s.frameRate(30);
+            s.noLoop();
+
+            axisSvg = d3.select("#" + parentId).append("svg")
+                .attr("width", s.width)
+                .attr("height", s.height)
+                .attr("id", "axis-svg")
+                .append("g");
+
+            axisSvg.append("g")
+                .attr('transform', 'translate(0,' + 0.95 * s.height + ')')
+                .classed('x-axis axis', true);
+
+            axisSvg.append("g")
+                .classed('y-axis axis', true);
+             
+            d3.csv(file)
+                .row(function (d) {
+                    return {
+                        // id: d['id'],
+                        company: d['company'],
+                        name: d['name'],
+                        ref: +d['ref'],
+                        date: +d['date'],
+                        percent: +d['percent'],
+                        location: d['location'],
+                        rating: +d['rating'],
+                        //  type: d['type'],
+                        origin: d['origin']
+                    };
+                })
+                .get(function (error, csv) {
+                    if (error) {
+                        console.log('error', error);
+                        return;
+                    }
+                    data = csv.filter(function (d, i) {
+                        return i < 100;
+                        //return true;
+                    });
+
+
+                    //           createButtons();
+                    //  setFilter(xFilter, yFilter);
+
+
+                    console.log('data', data);
+
+
+                    //             simulation.stop();
+                    //             for (var i = 0; i < 150; ++i){
+                    //                 simulation.tick();
+                    //             } 
+                    //             simulation.restart();
+
+                    ready = true;
+                    s.redraw();
+                });
+
+
+       
+
+        };
+
+        s.draw = function () {
+            s.background(0);
+            s.fill(255);
+            s.rect(x, y, 50, 50);
+        };
+    };
+
+
+    //p5 instance mode
+    var myp5 = new p5(sketch, parentId);
+
+}
