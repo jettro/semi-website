05d640ac87b55e61004ae9b69ad60e6a6eeadf06
Vledder <remi@studiospindle.nl>
2019-02-25 11:38:44 +0100

    working concept with fade-in animation when loading results

--- js-src/modules/search.js
+++ js-src/modules/search.js
@@ -1,7 +1,11 @@
 import utils from '../utilities/utils';
 
-// TODO: refactor this const into functions
+// TODO: refactor this const into functions or config
 const [resultListContainerElement] = document.getElementsByClassName('search-result-list');
+const [searchResultTemplateElement] = document.getElementsByClassName('search-result');
+const [pagination] = document.getElementsByClassName('pagination');
+const nextButtonClassName = 'pagination__button-next';
+const previousButtonClassName = 'pagination__button-previous';
 
 /**
  * @desc set title and URL
@@ -40,26 +44,15 @@ const makeResultContainer = function(template) {
 };
 
 /**
- *
- * @param response
+ * @desc remove all list items except the first since that's the template
  */
-const generateSearchResultsList = function(response) {
-  const [searchResultTemplateElement] = document.getElementsByClassName('search-result');
-  const { items } = response;
-
-  const hasResults = typeof items !== 'undefined';
-  if (hasResults) {
-    items.forEach(item => {
-      const clone = makeResultContainer(searchResultTemplateElement);
-      setResultHead(clone, item.htmlTitle, item.link, item.htmlFormattedUrl);
-      setResultSnippet(clone, item.htmlSnippet);
-      resultListContainerElement.appendChild(clone);
-    });
-  }
+const removeSearchResultsListItems = function() {
+  const results = resultListContainerElement.getElementsByClassName('search-result');
+  while (results[1]) results[1].parentNode.removeChild(results[1]);
 };
 
 /**
- *
+ * @desc Hide markup containing no results info
  */
 const hideNoResultsInfo = function() {
   const [noResultsElement] = document.getElementsByClassName('jsNoSearchResults');
@@ -80,30 +73,60 @@ const showNumberOfResults = function(response) {
   }
 };
 
+/**
+ *
+ * @param response
+ * @returns {*}
+ */
+const searchResultsListItems = function(response) {
+  const { items } = response;
+  const hasResults = typeof items !== 'undefined';
+  if (hasResults) {
+    const myMap = new Map();
+    items.forEach((item, i) => {
+      const clone = makeResultContainer(searchResultTemplateElement);
+      setResultHead(clone, item.htmlTitle, item.link, item.htmlFormattedUrl);
+      setResultSnippet(clone, item.htmlSnippet);
+      myMap.set(i, clone);
+    });
+    return myMap;
+  }
+};
+
+/**
+ *
+ * @param response
+ */
+const generateResults = function(response) {
+  const searchResultListItems = searchResultsListItems(response);
+  searchResultListItems.forEach((item) => {
+    resultListContainerElement.appendChild(item);
+  });
+};
 
 /**
  * @desc show pagination, add index to next and previous button
  * @param response
  */
-const pagination = function(response) {
-  const nextButtonClassName = 'pagination__button-next';
-  const previousButtonClassName = 'pagination__button-previous';
-  const [pagination] = document.getElementsByClassName('pagination');
+const generatePagination = function(response) {
   const [nextButton] = document.getElementsByClassName(nextButtonClassName);
   const [previousButton] = document.getElementsByClassName(previousButtonClassName);
-
   const queries = response.queries;
   const nextPage = queries.nextPage;
   const previousPage = queries.previousPage;
-
   if (pagination) {
     pagination.style.display = 'flex';
     if (nextPage) {
+      nextButton.style.display = 'flex';
       nextButton.dataset.goToPageIndex = nextPage[0].startIndex;
+    } else {
+      nextButton.style.display = 'none';
     }
     if (previousPage) {
       previousButton.style.display = 'flex';
       previousButton.dataset.goToPageIndex = previousPage[0].startIndex;
+    } else {
+      previousButton.style.display = 'none';
     }
   }
 };
@@ -112,7 +135,7 @@ const pagination = function(response) {
  *
  * @param query
  */
-const showUpdateSearchbox = function(query) {
+const showUpdatedSearchbox = function(query) {
   const searchBox = document.getElementById('search-knowledgebase');
   if (utils.elementExists(searchBox)) {
     searchBox.value = decodeURIComponent(`${query}`.replace(/\+/g, '%20'));
@@ -145,62 +168,60 @@ const getJSONResults = function(url) {
 
 export default function() {
 
+  // initially on pageload populate page using XHR
   const queryString = utils.getParameterByName('search');
-
-  // on page load
   if (queryString !== null && queryString !== '') {
 
     const urlParamIndex = window.location.hash.substring(1);
     const startIndex = urlParamIndex !== '' ? urlParamIndex : 1;
 
-    const ajaxPromise = getJSONResults(
+    const searchResultsPromise = getJSONResults(
       `https://www.googleapis.com/customsearch/v1` +
       `?key=${process.env.GOOGLE_API_KEY}` +
       `&cx=${process.env.GOOGLE_ENGINE}` +
       `&q=${queryString}&hl=en&start=${startIndex}`);
 
-    ajaxPromise.then(function(response) {
-                 hideNoResultsInfo();
-                 showNumberOfResults(response);
-                 generateSearchResultsList(response);
-                 pagination(response);
-                 showUpdateSearchbox(queryString);
-               })
-               .catch(function(e){ console.info(e); });
+    searchResultsPromise.then(function(response) {
+       hideNoResultsInfo();
+       showNumberOfResults(response);
+       generateResults(response);
+       resultListContainerElement.classList.add('search-result-list--fade-in');
+       generatePagination(response);
+       showUpdatedSearchbox(queryString);
+     })
+     .catch(function(e){
+       console.info(e);
+     });
   }
 
-  // when you click on a next or previous button it should change
+  // when pagination is clicked re-generate using XHR
+  if (utils.elementExists(pagination)) {
+    pagination.addEventListener('click', function(e) {
+      if (e.target.classList.contains(nextButtonClassName) || e.target.classList.contains(previousButtonClassName)) {
+        const newStartIndex = e.target.dataset.goToPageIndex;
+        const newSearchResultsPromise = getJSONResults(
+          `https://www.googleapis.com/customsearch/v1` +
+          `?key=${process.env.GOOGLE_API_KEY}` +
+          `&cx=${process.env.GOOGLE_ENGINE}` +
+          `&q=${queryString}&hl=en&start=${newStartIndex}`);
 
-  // when the hash is changed
-  // fade out the existing content
-  // fade in new content
-  // window.dispatchEvent(new HashChangeEvent('hashchange'));
-  // window.onhashchange = function() {
-  //   console.log('this hash is changed!');
-  // }
+        resultListContainerElement.classList.remove('search-result-list--fade-in');
+
+        newSearchResultsPromise.then(function(response) {
+          removeSearchResultsListItems();
+          // timeout for animation to finish
+          setTimeout(function(){
+            generateResults(response);
+            resultListContainerElement.classList.add('search-result-list--fade-in');
+            generatePagination(response);
+          }, 250);
+
+          window.location.hash = newStartIndex;
+        })
+        .catch(function(e){
+          console.info(e);
+        });
+      }
+    }, true)
+  }
 };
-
-
-
-//   const [noResultsElement] = document.getElementsByClassName('jsNoSearchResults');
-//   const [searchReturnsForbiddenElement] = document.getElementsByClassName('js-search-google-403');
-//
-//   const httpStatusOk = this.status === 200;
-//   const httpStatusForbidden = this.status === 403;
-//   const requestReadyStateDone = this.readyState === 4;
-//
-//   const requestDone = httpStatusOk && requestReadyStateDone;
-//
-//   if (requestDone) {
-//
-//   } else if (utils.elementExists(noResultsElement) && noResultsElement.style.display === 'none') {
-//     noResultsElement.style.display = 'flex';
-//   }
-// } else if (httpStatusForbidden) {
-//   if (searchReturnsForbiddenElement) {
-//     if (utils.elementExists(noResultsElement)) {
-//       noResultsElement.style.display = 'none';
-//     }
-//     searchReturnsForbiddenElement.style.display = 'flex';
-//   }
-// }
